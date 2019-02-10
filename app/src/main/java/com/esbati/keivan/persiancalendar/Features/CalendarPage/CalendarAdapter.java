f0d05ba07522886d445d233720921f59553e0479
Keivan Esbati <Keivan.Esbati@gmail.com>
2019-02-10 15:56:04 +0330

    Refactor calendar adapter moving excess logic to its fragment

--- app/src/main/java/com/esbati/keivan/persiancalendar/Features/CalendarPage/CalendarAdapter.java
+++ app/src/main/java/com/esbati/keivan/persiancalendar/Features/CalendarPage/CalendarAdapter.java
@@ -32,111 +32,103 @@ import ir.smartlab.persindatepicker.util.PersianCalendar;
 
 public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.DayHolder> {
 
-    private HomeFragment mFragment;
-    private Context mContext;
     private int mDisplayedYear;
     private int mDisplayedMonth;
-    private int mToday;
     private ArrayList<CalendarDay> mDays;
-
-    private boolean containToday;
-    private boolean isLeapYear;
-    private int dayOfWeek;
+    private OnCalendarClickListener mListener;
 
 
-    public CalendarAdapter(Fragment fragment, int year, int month) {
+    public interface OnCalendarClickListener {
+        void onCalendarClick(CalendarDay day);
+    }
+
+    void setOnCalendarClickListener(OnCalendarClickListener listener) {
+        this.mListener = listener;
+    }
+
+    CalendarAdapter(int year, int month, ArrayList<CalendarDay> days) {
         super();
 
-        mFragment = (HomeFragment) fragment;
-        mContext = fragment.getActivity();
         mDisplayedYear = year;
         mDisplayedMonth = month;
 
-        PersianCalendar thisYearCalendar = new PersianCalendar();
-        containToday = thisYearCalendar.getPersianYear() == mDisplayedYear && thisYearCalendar.getPersianMonth() == mDisplayedMonth;
-        mToday = thisYearCalendar.getPersianDay();
-
-        thisYearCalendar.setPersianDate(mDisplayedYear, mDisplayedMonth, 1);
-        isLeapYear = thisYearCalendar.isPersianLeapYear();
-        dayOfWeek = thisYearCalendar.getPersianWeekDay() % 7;
-
-        prepareDays();
+        mDays = days;
     }
 
-    public void prepareDays(){
+    class DayHolder extends RecyclerView.ViewHolder{
 
-        mDays = new ArrayList();
+        TextView mCalendarDay;
+        TextView mGoogleEvent;
 
-        int currentMonthDays = Constants.daysOfMonth[(mDisplayedMonth - 1)];
-        int previousMonthDays = Constants.daysOfMonth[(mDisplayedMonth - 2 + 12) % 12];
-
-        //Add Extra Day in Case of Leap Year
-        if(isLeapYear && mDisplayedMonth == 12)
-            currentMonthDays++;
-
-        //Add Extra Day to Previous Month in Case of Leap Year
-        if(mDisplayedMonth == 1){
-            PersianCalendar lastYearCalendar = new PersianCalendar();
-            lastYearCalendar.setPersianDate(mDisplayedYear - 1, mDisplayedMonth, 1);
-
-            if(lastYearCalendar.isPersianLeapYear())
-                previousMonthDays++;
-        }
-
-        //Add Trailing Days from Last Month if Needed
-        if(dayOfWeek > 0)
-            for(int i = dayOfWeek - 1 ; i >= 0 ; i--){
-                mDays.add(new CalendarDay(previousMonthDays - i));
-            }
-
-        //Add Month Days
-        for(int i = 1; i <= currentMonthDays ; i++){
-            CalendarDay day = new CalendarDay(new PersianCalendar().setPersianDate(mDisplayedYear, mDisplayedMonth, i));
-            day.isToday = (i == mToday && containToday);
-            day.isCurrentMonth = true;
-
-            //Get Events for Current Day
-            day.mCalendarEvents = CalendarHelper.getEvents(day.mPersianDate);
-            day.mGoogleEvents = GoogleCalendarHelper.getEvents(day.mPersianDate);
-
-            if(day.mPersianDate.getPersianWeekDay() == 6)
-                day.isHoliday = true;
-            else if(day.mCalendarEvents != null)
-                for(CalendarEvent calendarEvent : day.mCalendarEvents)
-                    if(calendarEvent.isHoliday){
-                        day.isHoliday = true;
-                        break;
-                    }
-
-            mDays.add(day);
-        }
-
-        //Add Leading Month Days
-        for(int i = 1 ; i <= mDays.size() % 7 ; i++){
-            mDays.add(new CalendarDay(i));
-        }
-    }
-
-    public static class DayHolder extends RecyclerView.ViewHolder{
-
-        public TextView mCalendarDay;
-        public TextView mGoogleEvent;
-        public View mCalendarBackground;
-
-        public DayHolder(View itemView) {
+        DayHolder(final View itemView) {
             super(itemView);
 
             mCalendarDay = (TextView)itemView.findViewById(R.id.calendar_day);
             mGoogleEvent = (TextView)itemView.findViewById(R.id.google_event);
-            mCalendarBackground = itemView.findViewById(R.id.calendar_background);
+
+            itemView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean isFocused) {
+                    if(isFocused)
+                        itemView.performClick();
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    CalendarDay day = mDays.get(getAdapterPosition());
+                    if(!day.isCurrentMonth)
+                        return;
+
+                    if(mListener != null)
+                        mListener.onCalendarClick(day);
+                }
+            });
+        }
+
+        void setupView(CalendarDay day){
+            Context context = itemView.getContext();
+            itemView.setEnabled(day.isCurrentMonth);
+
+            mCalendarDay.setText(String.valueOf(day.mDayNo));
+            if(day.mGoogleEvents != null && day.mGoogleEvents.size() > 0){
+                if(!TextUtils.isEmpty(day.mGoogleEvents.get(0).mTITLE))
+                    mGoogleEvent.setText(day.mGoogleEvents.get(0).mTITLE);
+                else
+                    mGoogleEvent.setText(R.string.event_no_title);
+            }
+
+            //Set Background Color
+            if(day.isHoliday && day.isToday){
+                mCalendarDay.setTextColor(context.getResources().getColor(android.R.color.holo_red_dark));
+                itemView.setBackgroundResource(R.drawable.bg_calendar_today);
+
+                mGoogleEvent.setTextColor(context.getResources().getColor(android.R.color.white));
+            } else if(day.isHoliday){
+                mCalendarDay.setTextColor(context.getResources().getColor(android.R.color.white));
+                itemView.setBackgroundResource(R.drawable.bg_calendar_holiday);
+
+                mGoogleEvent.setTextColor(context.getResources().getColor(android.R.color.white));
+            } else if(day.isToday){
+                mCalendarDay.setTextColor(context.getResources().getColor(android.R.color.white));
+                itemView.setBackgroundResource(R.drawable.bg_calendar_today);
+
+                mGoogleEvent.setTextColor(context.getResources().getColor(android.R.color.white));
+            } else if(day.isCurrentMonth) {
+                mCalendarDay.setTextColor(context.getResources().getColor(android.R.color.black));
+                itemView.setBackgroundResource(ColorHelper.getSeasonDrawableResource(mDisplayedMonth));
+
+                mGoogleEvent.setTextColor(context.getResources().getColor(android.R.color.black));
+            } else {
+                mCalendarDay.setTextColor(context.getResources().getColor(android.R.color.white));
+                itemView.setBackgroundColor(context.getResources().getColor(R.color.lighter_gray));
+
+                itemView.setEnabled(false);
+                mGoogleEvent.setTextColor(context.getResources().getColor(android.R.color.white));
+            }
         }
     }
-
-    @Override
-    public int getItemCount() {
-        return mDays == null ? 0 : mDays.size();
-    }
-
     @Override
     public DayHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_calendar, parent, false);
@@ -144,73 +136,21 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.DayHol
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
             if(PreferencesHelper.isOptionActive(PreferencesHelper.KEY_ANIMATION_SELECTION, false))
                 view.setStateListAnimator(AnimatorInflater.loadStateListAnimator(
-                        mFragment.getContext()
+                        parent.getContext()
                         , R.animator.cell_animator)
                 );
 
-        DayHolder dh = new DayHolder(view);
-        return dh;
+        return new DayHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final DayHolder holder, final int position) {
-        final CalendarDay mDay = mDays.get(position);
-        holder.itemView.setEnabled(mDay.isCurrentMonth);
+        CalendarDay day = mDays.get(position);
+        holder.setupView(day);
+    }
 
-        holder.mCalendarDay.setText(String.valueOf(mDay.mDayNo));
-        if(mDay.mGoogleEvents != null && mDay.mGoogleEvents.size() > 0){
-            if(!TextUtils.isEmpty(mDay.mGoogleEvents.get(0).mTITLE))
-                holder.mGoogleEvent.setText(mDay.mGoogleEvents.get(0).mTITLE);
-            else
-                holder.mGoogleEvent.setText(R.string.event_no_title);
-        }
-
-        //Set Background Color
-        if(mDay.isHoliday && mDay.isToday){
-            holder.mCalendarDay.setTextColor(mContext.getResources().getColor(android.R.color.holo_red_dark));
-            holder.itemView.setBackgroundResource(R.drawable.bg_calendar_today);
-
-            holder.mGoogleEvent.setTextColor(mContext.getResources().getColor(android.R.color.white));
-        } else if(mDay.isHoliday){
-            holder.mCalendarDay.setTextColor(mContext.getResources().getColor(android.R.color.white));
-            holder.itemView.setBackgroundResource(R.drawable.bg_calendar_holiday);
-
-            holder.mGoogleEvent.setTextColor(mContext.getResources().getColor(android.R.color.white));
-        } else if(mDay.isToday){
-            holder.mCalendarDay.setTextColor(mContext.getResources().getColor(android.R.color.white));
-            holder.itemView.setBackgroundResource(R.drawable.bg_calendar_today);
-
-            holder.mGoogleEvent.setTextColor(mContext.getResources().getColor(android.R.color.white));
-        } else if(mDay.isCurrentMonth) {
-            holder.mCalendarDay.setTextColor(mContext.getResources().getColor(android.R.color.black));
-            holder.itemView.setBackgroundResource(ColorHelper.getSeasonDrawableResource(mDisplayedMonth));
-
-            holder.mGoogleEvent.setTextColor(mContext.getResources().getColor(android.R.color.black));
-        } else {
-            holder.mCalendarDay.setTextColor(mContext.getResources().getColor(android.R.color.white));
-            holder.itemView.setBackgroundColor(mContext.getResources().getColor(R.color.lighter_gray));
-
-            holder.itemView.setEnabled(false);
-            holder.mGoogleEvent.setTextColor(mContext.getResources().getColor(android.R.color.white));
-        }
-
-        holder.itemView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean isFocused) {
-                if(isFocused)
-                    holder.itemView.performClick();
-            }
-        });
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!mDay.isCurrentMonth)
-                    return;
-
-                SoundManager.getInstance().playSound(mDay.mDayNo);
-                mFragment.showDate(mDay, true, false);
-            }
-        });
+    @Override
+    public int getItemCount() {
+        return mDays == null ? 0 : mDays.size();
     }
 }
--- app/src/main/java/com/esbati/keivan/persiancalendar/Features/CalendarPage/CalendarFragment.java
+++ app/src/main/java/com/esbati/keivan/persiancalendar/Features/CalendarPage/CalendarFragment.java
@@ -9,7 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.esbati.keivan.persiancalendar.Components.SoundManager;
+import com.esbati.keivan.persiancalendar.Features.Home.HomeFragment;
+import com.esbati.keivan.persiancalendar.POJOs.CalendarDay;
+import com.esbati.keivan.persiancalendar.POJOs.CalendarEvent;
 import com.esbati.keivan.persiancalendar.R;
+import com.esbati.keivan.persiancalendar.Repository.CalendarHelper;
+import com.esbati.keivan.persiancalendar.Repository.GoogleCalendarHelper;
+import com.esbati.keivan.persiancalendar.Utils.Constants;
+
+import java.util.ArrayList;
+
+import ir.smartlab.persindatepicker.util.PersianCalendar;
 
 /**
  * Created by asus on 11/17/2016.
@@ -27,10 +38,18 @@ public class CalendarFragment extends Fragment {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+
     public void refreshCalendar(int newYear){
         mYear = newYear;
 
-        mAdapter = new CalendarAdapter(getParentFragment(), mYear, mMonth);
+        mAdapter = new CalendarAdapter(mYear, mMonth, prepareDays(mYear, mMonth));
+        ((CalendarAdapter) mAdapter).setOnCalendarClickListener(new CalendarAdapter.OnCalendarClickListener() {
+            @Override
+            public void onCalendarClick(CalendarDay day) {
+                SoundManager.getInstance().playSound(day.mDayNo);
+                ((HomeFragment) getParentFragment()).showDate(day, true, false);
+            }
+        });
         mRecyclerView.setAdapter(mAdapter);
     }
 
@@ -57,7 +76,14 @@ public class CalendarFragment extends Fragment {
         mRecyclerView = (RecyclerView)rootView.findViewById(R.id.list);
         //mRecyclerView.setNestedScrollingEnabled(false);
 
-        mAdapter = new CalendarAdapter(getParentFragment(), mYear, mMonth);
+        mAdapter = new CalendarAdapter(mYear, mMonth, prepareDays(mYear, mMonth));
+        ((CalendarAdapter) mAdapter).setOnCalendarClickListener(new CalendarAdapter.OnCalendarClickListener() {
+            @Override
+            public void onCalendarClick(CalendarDay day) {
+                SoundManager.getInstance().playSound(day.mDayNo);
+                ((HomeFragment) getParentFragment()).showDate(day, true, false);
+            }
+        });
         mRecyclerView.setAdapter(mAdapter);
 
         mLayoutManager = new GridLayoutManager(getActivity(), 7, GridLayoutManager.VERTICAL, false);
@@ -73,5 +99,68 @@ public class CalendarFragment extends Fragment {
         CalendarFragment fragment = new CalendarFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public ArrayList<CalendarDay> prepareDays(int year, int month){
+
+        PersianCalendar calendar = new PersianCalendar();
+        boolean containToday = calendar.getPersianYear() == year && calendar.getPersianMonth() == month;
+        int mToday = calendar.getPersianDay();
+        calendar.setPersianDate(year, month, 1);
+        boolean isLeapYear = calendar.isPersianLeapYear();
+        int dayOfWeek = calendar.getPersianWeekDay() % 7;
+
+        ArrayList<CalendarDay> days = new ArrayList<>();
+
+        int currentMonthDays = Constants.daysOfMonth[(month - 1)];
+        int previousMonthDays = Constants.daysOfMonth[(month - 2 + 12) % 12];
+
+        //Add Extra Day in Case of Leap Year
+        if(isLeapYear && month == 12)
+            currentMonthDays++;
+
+        //Add Extra Day to Previous Month in Case of Leap Year
+        if(month == 1){
+            PersianCalendar lastYearCalendar = new PersianCalendar();
+            lastYearCalendar.setPersianDate(year - 1, month, 1);
+
+            if(lastYearCalendar.isPersianLeapYear())
+                previousMonthDays++;
+        }
+
+        //Add Trailing Days from Last Month if Needed
+        if(dayOfWeek > 0)
+            for(int i = dayOfWeek - 1 ; i >= 0 ; i--){
+                days.add(new CalendarDay(previousMonthDays - i));
+            }
+
+        //Add Month Days
+        for(int i = 1; i <= currentMonthDays ; i++){
+            CalendarDay day = new CalendarDay(new PersianCalendar().setPersianDate(year, month, i));
+            day.isToday = (i == mToday && containToday);
+            day.isCurrentMonth = true;
+
+            //Get Events for Current Day
+            day.mCalendarEvents = CalendarHelper.getEvents(day.mPersianDate);
+            day.mGoogleEvents = GoogleCalendarHelper.getEvents(day.mPersianDate);
+
+            if(day.mPersianDate.getPersianWeekDay() == 6)
+                day.isHoliday = true;
+            else if(day.mCalendarEvents != null)
+                for(CalendarEvent calendarEvent : day.mCalendarEvents)
+                    if(calendarEvent.isHoliday){
+                        day.isHoliday = true;
+                        break;
+                    }
+
+            days.add(day);
+        }
+
+        //Add Leading Month Days
+        for(int i = 1 ; i <= days.size() % 7 ; i++){
+            days.add(new CalendarDay(i));
+        }
+
+        return days;
     }
 }
