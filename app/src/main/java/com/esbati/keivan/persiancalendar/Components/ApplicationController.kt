0d05eef6a9f6e7cb29271d524cba9afa5a739ec0
Keivan Esbati <keivan.esbati@gmail.com>
2019-02-22 11:15:47 +0330

    Replace OneSignal with FCM

--- app/src/main/java/com/esbati/keivan/persiancalendar/Components/ApplicationController.kt
+++ app/src/main/java/com/esbati/keivan/persiancalendar/Components/ApplicationController.kt
@@ -3,14 +3,11 @@ package com.esbati.keivan.persiancalendar.Components
 import android.app.Application
 import android.content.Context
 import android.content.Intent
-
 import com.crashlytics.android.Crashlytics
-import com.esbati.keivan.persiancalendar.R
 import com.esbati.keivan.persiancalendar.Features.Notification.ApplicationService
 import com.esbati.keivan.persiancalendar.Features.Notification.NotificationUpdateService
+import com.esbati.keivan.persiancalendar.R
 import com.esbati.keivan.persiancalendar.Utils.AndroidUtilities
-import com.onesignal.OneSignal
-
 import io.fabric.sdk.android.Fabric
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
@@ -25,7 +22,6 @@ class ApplicationController : Application() {
 
         SoundManager.init()
         Fabric.with(this, Crashlytics())
-        OneSignal.startInit(this).init()
         CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/IRANSans(FaNum).ttf")
                 .setFontAttrId(R.attr.fontPath)
