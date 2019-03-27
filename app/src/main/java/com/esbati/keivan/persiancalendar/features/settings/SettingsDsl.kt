29aedbf550192a6b8499172dd498eef433d27671
Keivan Esbati <keivan.esbati@gmail.com>
2019-03-28 03:13:56 +0430

    Simplify Settings Dsl by making member functions into extensions

--- app/src/main/java/com/esbati/keivan/persiancalendar/features/settings/SettingsDsl.kt
+++ app/src/main/java/com/esbati/keivan/persiancalendar/features/settings/SettingsDsl.kt
@@ -3,6 +3,7 @@ package com.esbati.keivan.persiancalendar.features.settings
 import android.content.Context
 import android.support.annotation.StringRes
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.esbati.keivan.persiancalendar.features.settings.cells.*
 
@@ -11,33 +12,27 @@ import com.esbati.keivan.persiancalendar.features.settings.cells.*
 annotation class ViewMarker
 
 @ViewMarker
-class Container(context: Context) : LinearLayout(context) {
+class Container(context: Context) : LinearLayout(context)
 
-    init {
-        orientation = LinearLayout.VERTICAL
-    }
-
-    private fun <V : View> initViewIntoGroup(view: V, init: V.() -> Unit): V {
-        view.init()
-        addView(view)
-        return view
-    }
-
-    fun header(@StringRes titleResId: Int) = header { title = context.getString(titleResId) }
-    fun header(init: HeaderCell.() -> Unit) = initViewIntoGroup(HeaderCell(context), init)
-    fun textCheck(init: TextCheckCell.() -> Unit) = initViewIntoGroup(TextCheckCell(context), init)
-    fun textSetting(init: TextSettingsCell.() -> Unit) = initViewIntoGroup(TextSettingsCell(context), init)
-    fun textInfo(init: TextInfoCell.() -> Unit) = initViewIntoGroup(TextInfoCell(context), init)
-    fun shadowDivider() = initViewIntoGroup(ShadowSectionCell(context)) {}
-
-    companion object {
-        fun container(context: Context, init: Container.() -> Unit): Container {
-            val container = Container(context)
-            container.init()
-            return container
-        }
-    }
+fun container(context: Context, init: Container.() -> Unit): Container {
+    val container = Container(context)
+    container.orientation = LinearLayout.VERTICAL
+    container.init()
+    return container
 }
+
+private fun <V : View> ViewGroup.initViewIntoGroup(view: V, init: V.() -> Unit): V {
+    view.init()
+    addView(view)
+    return view
+}
+
+fun Container.header(@StringRes titleResId: Int) = header { title = context.getString(titleResId) }
+fun Container.header(init: HeaderCell.() -> Unit) = initViewIntoGroup(HeaderCell(context), init)
+fun Container.textCheck(init: TextCheckCell.() -> Unit) = initViewIntoGroup(TextCheckCell(context), init)
+fun Container.textSetting(init: TextSettingsCell.() -> Unit) = initViewIntoGroup(TextSettingsCell(context), init)
+fun Container.textInfo(init: TextInfoCell.() -> Unit) = initViewIntoGroup(TextInfoCell(context), init)
+fun Container.shadowDivider() = initViewIntoGroup(ShadowSectionCell(context)) {}
 
 fun View.onClick(action: (View) -> Unit) {
     setOnClickListener(action)
