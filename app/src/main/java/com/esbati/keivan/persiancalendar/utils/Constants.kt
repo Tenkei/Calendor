7b224a698e2d2d0bf03820ad8c58a28b31badec0
Ali Rezaiyan <alirezaiyann@gmail.com>
2019-07-08 11:03:37 +0430

    The day info is set to the tile

--- app/src/main/java/com/esbati/keivan/persiancalendar/utils/Constants.kt
+++ app/src/main/java/com/esbati/keivan/persiancalendar/utils/Constants.kt
@@ -1,5 +1,7 @@
 package com.esbati.keivan.persiancalendar.utils
 
+import com.esbati.keivan.persiancalendar.R
+
 /**
  * Created by Keivan Esbati on 11/18/2016.
  */
@@ -11,4 +13,19 @@ object Constants {
 
     val weekdays_en = arrayOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
     val months_en = arrayOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+    val daysIcon_fa = intArrayOf(
+        0,
+        R.drawable.day1, R.drawable.day2, R.drawable.day3,
+        R.drawable.day4, R.drawable.day5, R.drawable.day6,
+        R.drawable.day7, R.drawable.day8, R.drawable.day9,
+        R.drawable.day10, R.drawable.day11, R.drawable.day12,
+        R.drawable.day13, R.drawable.day14, R.drawable.day15,
+        R.drawable.day16, R.drawable.day17, R.drawable.day18,
+        R.drawable.day19, R.drawable.day20, R.drawable.day21,
+        R.drawable.day22, R.drawable.day23, R.drawable.day24,
+        R.drawable.day25, R.drawable.day26, R.drawable.day27,
+        R.drawable.day28, R.drawable.day29, R.drawable.day30,
+        R.drawable.day31
+    )
 }
